d41eb0d6c964a12f7ee0f66c28c1f7746413ef0f
Elvis Farias <elvisrfarias1@gmail.com>
2022-01-08 23:23:51 -0300

    coloquei a pasta como index

--- calculadora.js
+++ calculadora.js
@@ -1,15 +1,17 @@
 function insert(num) {
-  let numero = document.getElementById('resultado').innerHTML;
-  document.getElementById('resultado').innerHTML = numero + num;
+  let numero = document.getElementById("resultado").innerHTML;
+  document.getElementById("resultado").innerHTML = numero + num;
 }
 function back() {
-  let resultado = document.getElementById('resultado').innerHTML;
-  document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+  let resultado = document.getElementById("resultado").innerHTML;
+  document.getElementById("resultado").innerHTML = resultado.substring(
+    0,
+    resultado.length - 1
+  );
 }
 function calcular() {
-  let resultado = document.getElementById('resultado').innerHTML;
-  if(resultado) 
-  {
-    document.getElementById('resultado').innerHTML = eval(resultado)
+  let resultado = document.getElementById("resultado").innerHTML;
+  if (resultado) {
+    document.getElementById("resultado").innerHTML = eval(resultado);
   }
 }
